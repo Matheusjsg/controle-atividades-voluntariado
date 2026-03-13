17caba43a86723b803ea78dcdb8c125ca6aa6122
Matheus Jesus <matheussantos930@gmail.com>
2026-03-13 19:33:45 -0300

    config variaveis

--- src/main/resources/db/migration/V2__create_table_voluntario.sql
+++ src/main/resources/db/migration/V2__create_table_voluntario.sql
@@ -1,11 +1,16 @@
 CREATE TABLE tb_voluntario (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     nome VARCHAR(150) NOT NULL,
     email VARCHAR(150),
     setor_id BIGINT NOT NULL,
     tipo_usuario VARCHAR(20),
 
-    CONSTRAINT fk_setor
+    CONSTRAINT fk_voluntario_setor
         FOREIGN KEY (setor_id)
-        REFERENCES tb_setor(id)
+        REFERENCES tb_setor(id),
+
+    CONSTRAINT uk_voluntario_nome UNIQUE (nome)
 );
+
+CREATE INDEX idx_voluntario_setor
+ON tb_voluntario(setor_id);
--- src/main/resources/db/migration/V3__create_table_atividade.sql
+++ src/main/resources/db/migration/V3__create_table_atividade.sql
@@ -1,12 +1,15 @@
 CREATE TABLE tb_atividade (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     data DATE NOT NULL,
     descricao TEXT,
     tempo_minutos INTEGER,
-    voluntario_id BIGINT,
-    status VARCHAR(20),
+    voluntario_id BIGINT NOT NULL,
+    status VARCHAR(20) DEFAULT 'PENDENTE',
 
-    CONSTRAINT fk_voluntario
+    CONSTRAINT fk_atividade_voluntario
         FOREIGN KEY (voluntario_id)
         REFERENCES tb_voluntario(id)
 );
+
+CREATE INDEX idx_atividade_voluntario
+ON tb_atividade(voluntario_id);
