5767179c06b969733ae5925e4c73b338a9b6d5f4
kaleb-tavares <kalebtavares@icloud.com>
2026-03-17 17:16:11 -0400

    fix(user): Ajusta porta do servidor e anotação do controller

--- src/main/java/com/abcaa/sistema_atividades/controller/UserController.java
+++ src/main/java/com/abcaa/sistema_atividades/controller/UserController.java
@@ -5,6 +5,7 @@ import com.abcaa.sistema_atividades.business.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<TbUser> saveUser(TbUser tb_user){
+    public ResponseEntity<TbUser> saveUser(@RequestBody TbUser tb_user){
         return ResponseEntity.ok(userService.saveUser(tb_user));
     }
 }
