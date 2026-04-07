569b4968eccc6a60401b881f62063a0429d0a9cc
Matheus Jesus <matheussantos930@gmail.com>
2026-04-07 10:41:07 -0300

    Feat: Implementing Pagination in Activities and Volunteers

--- src/main/java/com/abcaa/sistema_atividades/business/repositories/ActivityRepository.java
+++ src/main/java/com/abcaa/sistema_atividades/business/repositories/ActivityRepository.java
@@ -2,6 +2,8 @@ package com.abcaa.sistema_atividades.business.repositories;
 
 import com.abcaa.sistema_atividades.business.entities.Activity;
 import com.abcaa.sistema_atividades.business.enums.ActivityStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.time.LocalDate;
 import java.util.List;
@@ -15,5 +17,10 @@ List<Activity> findByVolunteerIdAndActivityStatus(Long volunteerId, ActivityStat
 List<Activity> findByVolunteerIdAndActivityStatusAndDateBetween(Long volunteerId, ActivityStatus status, LocalDate startDate, LocalDate endDate);
 List<Activity> findByVolunteerIdAndActivityStatusAndDateGreaterThanEqual(Long volunteerId, ActivityStatus status, LocalDate startDate);
 List<Activity> findByVolunteerIdAndActivityStatusAndDateLessThanEqual(Long volunteerId, ActivityStatus status, LocalDate endDate);
+
+
+    Page<Activity> findAll(Pageable pageable);
+    Page<Activity> findByVolunteerId(Long volunteerId, Pageable pageable);
+    Page<Activity> findByActivityStatus(ActivityStatus status, Pageable pageable);
 
 }
--- src/main/java/com/abcaa/sistema_atividades/business/repositories/VolunteerRepository.java
+++ src/main/java/com/abcaa/sistema_atividades/business/repositories/VolunteerRepository.java
@@ -1,6 +1,8 @@
 package com.abcaa.sistema_atividades.business.repositories;
 
 import com.abcaa.sistema_atividades.business.entities.Volunteer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +11,6 @@ import java.util.Optional;
 public interface VolunteerRepository extends JpaRepository<Volunteer, Long>{
     List<Volunteer> findAllByOrderByNameAsc();
     Optional<Volunteer> findByEmail(String email);
+
+    Page<Volunteer> findAll(Pageable pageable);
 }
